e5baf99631b9512f71ccebc1934d592def016f88
Durgesh Tambe <durgesh.tambe@varahitechnologies.com>
2020-04-28 12:07:54 +0530

    Updates

---
+++ precache-manifest.312845e6c8d3bc235d8fef70cba1a11f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e91bc453db1e8ab58f9ef2ca4fba7e",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "a2b58c24b77ef69374a3",
+    "url": "/production/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0311b2171a9e3021eece",
+    "url": "/production/static/js/2.b9eeac10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/production/static/js/2.b9eeac10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b58c24b77ef69374a3",
+    "url": "/production/static/js/main.143333ea.chunk.js"
+  },
+  {
+    "revision": "82b5cc9c15a686e8c527",
+    "url": "/production/static/js/runtime-main.d3942a18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/production/static/media/logo.5d5d9eef.svg"
+  }
+]);
